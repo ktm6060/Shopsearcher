829e71a75e06ea4c14587cf31ca38f429cdd54d1
ktm6060 <ktm6060mc@gmail.com>
2020-08-11 22:55:11 -0500

    Created PlotSearchUI

--- src/ktm6060/shopsearcher/ui/PlotSearchUI.java
+++ src/ktm6060/shopsearcher/ui/PlotSearchUI.java
@@ -1,46 +1,36 @@
 package ktm6060.shopsearcher.ui;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import ktm6060.shopsearcher.objects.ShopItem;
-import ktm6060.shopsearcher.utils.Tools;
 import ktm6060.shopsearcher.utils.Utils;
 
-public class ItemSearchUI extends GUI {
+public class PlotSearchUI extends GUI {
 	
 	private static Inventory inv;
 	public static String inventoryName;
 	private static int invRows = 6;
 	private static int invBoxes = invRows * 9;
-	private static int currPage = 1;
-	public static int numPages = 0;
-	private static ArrayList<ShopItem> shopItems = new ArrayList<ShopItem>();
 	
 	public static void initialize() {
-		inventoryName = Utils.format("&8Item Search (Page " + currPage + " of " + numPages + ")");
+		inventoryName = Utils.format("&8Plot Search");
 		inv = Bukkit.createInventory(null, invBoxes);
 	}
-
-	public static Inventory GUI(Player player, int page) {
-		currPage = page;
-		return GUI(player);
-	}
-
+	
 	public static Inventory GUI(Player player) {
-		numPages = shopItems.size() / 45;
-		numPages += shopItems.size() % 45 > 0 ? 1 : 0;
-		inventoryName = Utils.format("&8Item Search (Page " + currPage + " of " + numPages + ")");
-		
+		return GUI(player, 0);
+	}
+	
+	public static Inventory GUI(Player player, int page) {
 		Inventory toReturnInventory = Bukkit.createInventory(null, invBoxes, inventoryName);
 		inv.clear();
 		
-		Tools.displayShopItemsOnly(inv, shopItems, currPage, 45);
-		Tools.setPageSwitchingIcons(inv, numPages, currPage);
+		String[] directions = {"NW", "NE", "SW", "SE", "N", "E", "S", "W"};
+		String[] directionsFull = {"North West", "North East", "South West", "South East", "North", "East", "South", "West"};
+		
+		// TODO build UI icons
 		
 		Utils.createItem(inv, "barrier", 1, 49, "&CGo Back");
 		
@@ -55,21 +45,10 @@ public class ItemSearchUI extends GUI {
 		else if (clicked.getItemMeta().getDisplayName().contains(Utils.format("Page ")))
 		{
 			int target = Integer.parseInt(clicked.getItemMeta().getDisplayName().substring(7));
-			
-			if (target < currPage)
-				currPage--;
-			else if (target > currPage)
-				currPage++;
-			
-			player.openInventory(ItemSearchUI.GUI(player, target));
+			player.openInventory(PlotSearchUI.GUI(player, target));
 		}
-		else
-		{
-			Tools.viewOffers(player, clicked, currPage);
-		}
+		
+		// TODO handle clicked icons for any plot
 	}
-	
-	public static void setShopItems(ArrayList<ShopItem> shopItems) {
-		ItemSearchUI.shopItems = shopItems;
-	}
+
 }
